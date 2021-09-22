c17ec7c9f25f6ee9c0c257f5f48a0f540c76ffdc
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-09-22 02:35:44 +0200

    example layout

--- client/src/App.js
+++ client/src/App.js
@@ -20,16 +20,19 @@ class App extends Component {
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+
+      // this.setState({ web3, accounts, contract: instance }, this.runExample);
+
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
@@ -50,21 +53,87 @@ class App extends Component {
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div>Loading Web3., accounts, and contract...</div>;
     }
+
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <header id="header">
+          <h1>Bookchain</h1>
+          <small>Track personal document using the Ethereum Blockchain</small>
+        </header>
+
+        <div id="formsContainer">
+          {/* FORM FOR DOCS SUBMISSION */}
+          <form>
+            <div className="formTitle">Authorize your Document</div>
+            <div>
+              <label>
+                File
+                <small>
+                  Only the hash of the document is retrieved to be stored; the
+                  actual contents are never stored anywhere.
+                </small>
+              </label>
+              <input type="file" />
+            </div>
+            <div>
+              <label>
+                Hash <small>The hash of the document</small>
+              </label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Author Name</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Title</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Email</label>
+              <input type="text" />
+            </div>
+            <div>
+              <button type="submit">Authorize it!</button>
+            </div>
+            <div>
+              <label style={{ textAlign: "center" }}> DONE </label>
+            </div>
+          </form>
+
+          {/* FORM FOR DOCS VERIFICATION */}
+          <form>
+            <div className="formTitle">
+              Pick a document to display its details from the Blockchain
+            </div>
+            <div>
+              <input type="file" />
+            </div>
+            <div>
+              <label>
+                Hash <small>The hash of the document</small>
+              </label>
+              <input type="text" />
+            </div>
+            <div>
+              <button>Get Doc Details from Bookchain</button>
+            </div>
+            <div id="details" className="docDetails">
+              <ul>
+                <li>Author Name: test1 </li>
+                <li>Title: test2 </li>
+                <li>Email: test3 </li>
+              </ul>
+            </div>
+          </form>
+          {/* DEBUG */}
+          <form>
+            <div> DEBUG </div>
+            <div> Storage value : {this.state.storageValue} </div>
+          </form>
+        </div>
       </div>
     );
   }
